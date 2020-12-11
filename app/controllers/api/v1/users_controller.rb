2f67489c7c30656a268b212e55fbfaa3005b1d1d
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-11 16:58:18 -0600

    jwt implemented, creating and logging in users working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::UsersController < ApplicationController
 
-  def show
-    user = User.find(1)
+  def create
+    
+    user = User.create(username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation], email: params[:email], pic: params[:pic], fav_genre: params[:fav_genre], fav_game: params[:fav_game])
+    
+    if user.save
+      payload = { user_id: user.id }
+      token = JWT.encode(payload, 'my_secret', 'HS256')
+        render json: { user: user, token: token}
+    else
+        render json: {error: user.errors.full_messages}, status: 401
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+
     render json: user
   end
+
 end
