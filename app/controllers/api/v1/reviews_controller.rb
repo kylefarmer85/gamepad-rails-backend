ab69b5d7eca05a886684794e37fd7a4540742cfe
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-14 16:52:33 -0600

    game reviews all sending json to FE

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,6 +2,12 @@ class Api::V1::ReviewsController < ApplicationController
 
   def game_reviews_all
     game_api_id = params[:game_api_id]
-    found_game = games.find_by (:game_api_id: game.game_api_id)
+    games = Game.all
+    found_game = games.find_by(game_api_id: game_api_id)
+    if found_game 
+      render json: {game_reviews: found_game.reviews}
+    else 
+      render json: { error: "No Reviews Yet" }
+    end
   end
 end
