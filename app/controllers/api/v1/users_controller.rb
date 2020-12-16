ae15da177f2f139799a6b7643ece2bddfb9ddeaa
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-16 15:19:00 -0600

    users show working, users destroy working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,18 @@ class Api::V1::UsersController < ApplicationController
   end
 
 
+  def show
+    user = User.find(params[:id])
+
+    if user
+      render json: user
+
+    else  
+      render json: {error: user.errors.full_messages}, status: 401
+    end
+  end
+
+
   def create
     
     user = User.create(username: params[:username], password: params[:password], password_confirmation: params[:password_confirmation], email: params[:email], pic: params[:pic], fav_genre: params[:fav_genre], fav_game: params[:fav_game])
