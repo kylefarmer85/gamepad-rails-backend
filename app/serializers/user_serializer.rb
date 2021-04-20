c76e8ad900d74f8aa9b076ff6d705883af636f27
kylefarmer85 <kylefarmer85@gmail.com>
2021-04-20 16:11:10 -0500

    refactor user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,19 +2,11 @@ class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   has_many :favorites
-  has_many :games, through: :favorites
+  has_many :games
   has_many :reviews
-
   has_many :comments
-
-  has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
-
-  has_many :followers, through: :received_follows, source: :follower
-
-  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
-  
-  has_many :followings, through: :given_follows, source: :followed_user
-
+  has_many :followers
+  has_many :followings
 
   attributes :id, :username, :email, :fav_genre, :fav_game, :fav_console, :photo 
 
